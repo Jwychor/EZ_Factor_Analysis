005b8178e3294f3b248efbb3c21d3e40ce107a55
Jwychor <53948645+Jwychor@users.noreply.github.com>
2019-09-17 20:49:33 -0500

    Update EZ-Factor-Analysis-Dashboard.r

--- EZ-Factor-Analysis-Dashboard.r
+++ EZ-Factor-Analysis-Dashboard.r
@@ -1,6 +1,6 @@
 ####Data Entry#####
 #replace 'name' with the name of a dataframe in your global environment
-dat <- os #<---- Replace
+dat <- name #<---- Replace
 #Then press cntrl + a, then cntrl + enter. The app should initialize.
 
 ####Packages####
@@ -22,65 +22,178 @@ round_df <- function(df, digits) {
 ####Application#####
 r2<-pca(dat,screePlot=T,nFactorMethod = 'eigen')
 
-
+#UI
 ui <- fluidPage(
   
   titlePanel('EZ Factor Analysis'),
   
     #Inputs
-  sidebarLayout(
-    sidebarPanel(
-    sliderInput(inputId='factor',
-      label = 'Number of Factors',
-      value = 2, min = 2, max = ncol(dat), step = 1),
-    
-    sliderInput(inputId='loadings',
-      label = 'Hide Eigenvalues Below',
-      value = 0.59, min = 0, max = 1, step=.01),
-    
-    selectInput(inputId='rotation',
-      label = 'Rotation',
-      choices=c('varimax','oblimin')),
-    
-    actionButton(inputId='button',
-      label = 'Run')),
-  
-  #Outputs
-  mainPanel(
-    plotOutput(outputId = 'scree',width = '100%')
-  )),
-  div(DT::dataTableOutput(outputId = 'factor.table'),
-      style = "font-size: 100%; width: 45%"),
-  
-  div(DT::dataTableOutput(outputId = 'eigen.table'),
-      style = "font-size: 100%; width: 95%")
+  tabsetPanel(
+    tabPanel("Factor",
+      sidebarLayout(
+        sidebarPanel(
+        sliderInput(inputId='factor',
+          label = 'Number of Factors',
+          value = 2, min = 1, max = ncol(dat), step = 1),
+        
+        sliderInput(inputId='loadings',
+          label = 'Hide Eigenvalues Below',
+          value = 0.59, min = 0, max = 1, step=.01),
+        
+        selectInput(inputId='rotation',
+          label = 'Rotation',
+          choices=c('varimax','oblimin')),
+        
+        actionButton(inputId='button',
+          label = 'Run')),
+      
+      #Outputs
+      mainPanel(
+        textOutput('scree.text'),
+        
+        plotOutput(outputId = 'scree',width = '100%')
+      )),
+      wellPanel(
+        textOutput('factor.text'),
+        
+        div(DT::dataTableOutput(outputId = 'factor.table'),
+          style = "font-size: 100%; width: 45%")
+      ),
+      
+      wellPanel(
+        textOutput('eigen.text'),
+        
+        div(DT::dataTableOutput(outputId = 'eigen.table'),
+          style = "font-size: 100%; width: 95%")
+      )
+    ),
+        tabPanel("Subscales",
+            sidebarLayout(
+              sidebarPanel(
+                
+              uiOutput('a.slider'),
+            
+              actionButton(inputId = 'button2',
+                label='Run')),
+            
+            mainPanel(
+              textOutput('alpha.text'),
+              
+              div(DT::dataTableOutput(outputId = 'alpha.table'),
+                style = "font-size: 90%; width: 45%")
+          )
+        ),
+        wellPanel(
+          textOutput('scale.text'),
+          
+          div(DT::dataTableOutput(outputId = 'scale.table'),
+            style = "font-size: 100%; width: 90%"),
+        
+        wellPanel(
+          textOutput('items.text'),
+          
+          div(DT::dataTableOutput(outputId = 'item.table'),
+              style = "font-size: 100%; width: 90%")
+        )
+      )
+    )
+  )
 )
-  
 
+
+#Server
 server <- function(input, output, session) {
   
   e1<-reactive({input$factor})
   e2<-reactive({input$loadings})
   e3<-reactive({input$rotation})
   
+  a1<-reactive({input$scale})
+  
+  e4<-eventReactive(input$button,{input$factor})
+  
+  e5<-eventReactive(input$button2,{input$scale})
+  
   r1<-eventReactive(input$button,{pca(dat,nFactorMethod='fixed',nFactors = e1(),hideLoadings=e2(),
                 screePlot=F,sortLoadings = T,eigen = T,rotation=e3(),
                 factorSummary = T)})
   
+  a2<-reactive({as.data.frame(r1()$loadings)})
+  
+  m1<-reactive({a2()[,c(1,(a1()+1))]})
+  
+  n1<-reactive({subset(m1(),!is.na(m1()[,2]))})
+  
+  m2<-reactive({n1()[,1]})
+  
+  alp<-eventReactive(input$button2,{alpha(
+    dat[,colnames(dat) %in% m2()]
+  )})
+  
   output$factor.table <- DT::renderDataTable({
-    DT::datatable(round_df(as.data.frame(r1()$loadings)[,-1],digits=3),
+    DT::datatable(round_df(as.data.frame(r1()$loadings),digits=3),
           options = list(pageLength = ncol(dat),
-                         lengthMenu = c(5,10,15,round(1/2*ncol(dat),0),ncol(dat))))
+            lengthMenu = c(5,10,15,round(1/2*ncol(dat),0),ncol(dat))))
   })
   
   output$eigen.table <- DT::renderDataTable({
-    DT::datatable(round_df(as.data.frame(r1()$factorStats$factorSummary)[,-1],digits=2),
+    DT::datatable(round_df(as.data.frame(r1()$factorStats$factorSummary),digits=2),
         options = list(pageLength = e1()))
+  })
+  
+  output$alpha.table <- DT::renderDataTable({
+    round_df(alp()$total,digits = 3)
+  })
+  
+  output$scale.table <- DT::renderDataTable({
+    DT::datatable(round_df(alp()$alpha.drop,digits = 3),
+        options = list(pageLength = 100))
+  })
+  
+  output$item.table <- DT::renderDataTable({
+    DT::datatable(round_df(alp()$item.stats,digits = 3),
+        options = list(pageLength = 100))
+  })
+  
+  output$a.slider<-renderUI({
+    tagList(
+      sliderInput('scale',
+        label='Subscale Number',
+        value = 1, min = 1, max = input$factor,step = 1)
+    )
   })
   
   output$scree <- renderPlot({
     r2$eigen
   })
+  
+  #Text
+  output$scree.text<- renderText({
+    'Scree Plot'
+  })
+  
+  output$factor.text<- renderText({
+    paste('Factor Loadings on Principal Component Analysis of ',
+          e4(),' Factors',sep='')
+  })
+  
+  output$eigen.text<- renderText({
+    paste('Eigen Value Information for Principal Component Analysis of ',
+          e4(),' Factors',sep='')
+  })
+  
+  output$alpha.text<- renderText({
+    paste("Cronbach's Alpha Criteria for Subscale ",e5(),sep='')
+  })
+  
+  output$scale.text<- renderText({
+    paste("Alpha if Item Missing for Subscale ",e5(),sep='')
+  })
+  
+  output$items.text<- renderText({
+    paste("Item Statistics for Subscale ",e5(),sep='')
+  })
 }
+
 
 shinyApp(ui, server)
