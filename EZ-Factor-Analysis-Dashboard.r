35dc0b595fc6ae5e5abad1447cca1c5bf8200955
Jwychor <53948645+Jwychor@users.noreply.github.com>
2019-09-24 14:27:41 -0500

    Update EZ-Factor-Analysis-Dashboard.r

--- EZ-Factor-Analysis-Dashboard.r
+++ EZ-Factor-Analysis-Dashboard.r
@@ -8,8 +8,8 @@ dat <- name #<---- Replace
 #packages that have not yet been installed
 require('dplyr')
 require('DT')
-require('jmv')
 require('psych')
+require('jmv')
 require('shiny')
 require('shinydashboard')
 round_df <- function(df, digits) {
@@ -21,7 +21,7 @@ round_df <- function(df, digits) {
 }
 
 ####Application#####
-r2<-pca(dat,screePlot=T,nFactorMethod = 'eigen',eigen=T)
+r2<-jmv::pca(dat,screePlot=T,nFactorMethod = 'eigen',eigen=T)
 
 a<-alpha(dat)
 
@@ -30,97 +30,98 @@ ui <- fluidPage(
   
   titlePanel('EZ Factor Analysis'),
   
-    #Inputs
+  #Inputs
   tabsetPanel(
     
     tabPanel("Factor",
              
-      sidebarLayout(
-        sidebarPanel(
-        sliderInput(inputId='factor',
-          label = 'Number of Factors',
-          value = 2, min = 1, max = ncol(dat), step = 1),
-        
-        sliderInput(inputId='loadings',
-          label = 'Hide Loadings Below',
-          value = 0.59, min = 0, max = 1, step=.01),
-        
-        selectInput(inputId='rotation',
-          label = 'Rotation',
-          choices=c('varimax','oblimin')),
-        
-        actionButton(inputId='button',
-          label = 'Run')),
-      
-      #Outputs
-      mainPanel(
-        htmlOutput('Scree.text'),
-        
-        plotOutput(outputId = 'scree',width = '100%')
-      )),
-      wellPanel(
-        htmlOutput('factor.text'),
-        
-        div(DT::dataTableOutput(outputId = 'factor.table'),
-          style = "font-size: 100%; width: 45%")
-      ),
-      wellPanel(
-        div(DT::dataTableOutput(outputId = 'eigen.table'),
-          style = "font-size: 100%; width: 95%")),
-      
-      sidebarLayout(
-        sidebarPanel(
-          htmlOutput('eigen.text'),
-            
-            tags$p(tags$h4(tags$strong('Initial Eigenvalue Table'))),
-            
-            div(DT::dataTableOutput(outputId = 'init.eigen'),
-              style = 'font-size: 100%; width: 95%')
-       ),
-
-        mainPanel(
-          htmlOutput('al.text'),
-          
-          tags$p(tags$h4(tags$strong("Scale Cronbach's Alpha"))),
-          
-          DT::dataTableOutput(outputId = 'al.table'),
-          
-          tags$p(tags$h4(tags$strong("Scale Item Statistics"))),
-          
-          DT::dataTableOutput(outputId = 'al.items')
-          
-      )
-      )
+             sidebarLayout(
+               sidebarPanel(
+                 sliderInput(inputId='factor',
+                             label = 'Number of Factors',
+                             value = 2, min = 1, max = ncol(dat), step = 1),
+                 
+                 sliderInput(inputId='loadings',
+                             label = 'Hide Loadings Below',
+                             value = 0.59, min = 0, max = 1, step=.01),
+                 
+                 selectInput(inputId='rotation',
+                             label = 'Rotation',
+                             choices=c('varimax','oblimin')),
+                 
+                 actionButton(inputId='button',
+                              label = 'Run')),
+               
+               #Outputs
+               mainPanel(
+                 htmlOutput('Scree.text'),
+                 
+                 plotOutput(outputId = 'scree',width = '100%')
+               )
+              ),
+             wellPanel(
+               htmlOutput('factor.text'),
+               
+               div(DT::dataTableOutput(outputId = 'factor.table'),
+                   style = "font-size: 100%; width: 45%")
+             ),
+             wellPanel(
+               div(DT::dataTableOutput(outputId = 'eigen.table'),
+                   style = "font-size: 100%; width: 95%")),
+             
+             sidebarLayout(
+               sidebarPanel(
+                 htmlOutput('eigen.text'),
+                 
+                 tags$p(tags$h4(tags$strong('Initial Eigenvalue Table'))),
+                 
+                 div(DT::dataTableOutput(outputId = 'init.eigen'),
+                     style = 'font-size: 100%; width: 95%')
+               ),
+               
+               mainPanel(
+                 htmlOutput('al.text'),
+                 
+                 tags$p(tags$h4(tags$strong("Scale Cronbach's Alpha"))),
+                 
+                 DT::dataTableOutput(outputId = 'al.table'),
+                 
+                 tags$p(tags$h4(tags$strong("Scale Item Statistics"))),
+                 
+                 DT::dataTableOutput(outputId = 'al.items')
+                 
+               )
+             )
     ),
-        tabPanel("Subscales",
-            sidebarLayout(
-              sidebarPanel(
-                
-              uiOutput('a.slider'),
-            
-              actionButton(inputId = 'button2',
-                label='Run')),
-            
-            mainPanel(
-              htmlOutput('alpha.text'),
-              
-              div(DT::dataTableOutput(outputId = 'alpha.table'),
-                style = "font-size: 90%; width: 45%")
-          )
-        ),
-        wellPanel(
-          htmlOutput('scale.text'),
-          
-          div(DT::dataTableOutput(outputId = 'scale.table'),
-            style = "font-size: 100%; width: 90%"),
-        
-        wellPanel(
-          htmlOutput('items.text'),
-          
-          div(DT::dataTableOutput(outputId = 'item.table'),
-              style = "font-size: 100%; width: 90%")
-        )
-      )
+    tabPanel("Subscales",
+             sidebarLayout(
+               sidebarPanel(
+                 
+                 uiOutput('a.slider'),
+                 
+                 actionButton(inputId = 'button2',
+                              label='Run')),
+               
+               mainPanel(
+                 htmlOutput('alpha.text'),
+                 
+                 div(DT::dataTableOutput(outputId = 'alpha.table'),
+                     style = "font-size: 90%; width: 45%")
+               )
+             ),
+             wellPanel(
+               htmlOutput('scale.text'),
+               
+               div(DT::dataTableOutput(outputId = 'scale.table'),
+                   style = "font-size: 100%; width: 90%"),
+               
+               wellPanel(
+                 htmlOutput('items.text'),
+                 
+                 div(DT::dataTableOutput(outputId = 'item.table'),
+                     style = "font-size: 100%; width: 90%")
+               )
+             )
     )
   )
 )
@@ -138,9 +139,9 @@ server <- function(input, output, session) {
   e4<-eventReactive(input$button,{input$factor})
   e5<-eventReactive(input$button2,{input$scale})
   
-  r1<-eventReactive(input$button,{pca(dat,nFactorMethod='fixed',nFactors = e1(),hideLoadings=e2(),
-                screePlot=F,sortLoadings = T,eigen = T,rotation=e3(),
-                factorSummary = T)})
+  r1<-eventReactive(input$button,{jmv::pca(dat,nFactorMethod='fixed',nFactors = e1(),hideLoadings=e2(),
+                                      screePlot=F,sortLoadings = T,eigen = T,rotation=e3(),
+                                      factorSummary = T)})
   
   a2<-reactive({as.data.frame(r1()$loadings)})
   
@@ -156,13 +157,13 @@ server <- function(input, output, session) {
   
   output$factor.table <- DT::renderDataTable({
     DT::datatable(round_df(as.data.frame(r1()$loadings),digits=3),
-          options = list(pageLength = ncol(dat),
-            lengthMenu = c(5,10,15,round(1/2*ncol(dat),0),ncol(dat))))
+                  options = list(pageLength = ncol(dat),
+                                 lengthMenu = c(5,10,15,round(1/2*ncol(dat),0),ncol(dat))))
   })
   
   output$eigen.table <- DT::renderDataTable({
     DT::datatable(round_df(as.data.frame(r1()$factorStats$factorSummary),digits=2),
-        options = list(pageLength = e1()))
+                  options = list(pageLength = e1()))
   })
   
   output$init.eigen <- DT::renderDataTable({
@@ -186,19 +187,19 @@ server <- function(input, output, session) {
   
   output$scale.table <- DT::renderDataTable({
     DT::datatable(round_df(alp()$alpha.drop,digits = 3),
-        options = list(pageLength = 100))
+                  options = list(pageLength = 100))
   })
   
   output$item.table <- DT::renderDataTable({
     DT::datatable(round_df(alp()$item.stats,digits = 3),
-        options = list(pageLength = 100))
+                  options = list(pageLength = 100))
   })
   
   output$a.slider<-renderUI({
     tagList(
       sliderInput('scale',
-        label='Subscale Number',
-        value = 1, min = 1, max = input$factor,step = 1)
+                  label='Subscale Number',
+                  value = 1, min = 1, max = input$factor,step = 1)
     )
   })
   
@@ -213,12 +214,12 @@ server <- function(input, output, session) {
   
   output$factor.text<- renderText({
     HTML(paste('<h4><B>Factor Loadings on Principal Component Analysis of ',
-          e4(),' Factors</B></h4>',sep=''))
+               e4(),' Factors</B></h4>',sep=''))
   })
   
   output$eigen.text<- renderText({
     HTML(paste('<h4><B>Eigen Value Information for Principal Component Analysis of ',
-          e4(),' Factors</h4></B>',sep=''))
+               e4(),' Factors</h4></B>',sep=''))
   })
   
   output$alpha.text<- renderText({
